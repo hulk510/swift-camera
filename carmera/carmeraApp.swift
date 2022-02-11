4b4038f0cd58e0cdc541b728637a8788179a58f6
hulk510 <dorian.51069@gmail.com>
2022-02-11 23:39:06 +0900

    Initial Commit

---
+++ carmera/carmeraApp.swift
@@ -0,0 +1,17 @@
+//
+//  carmeraApp.swift
+//  carmera
+//
+//  Created by 後藤遥 on 2022/02/11.
+//
+
+import SwiftUI
+
+@main
+struct carmeraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
